4e95d75eb019afd647aef7cdf3ab1ef4e356659f
fit <arrowhead98765@gmail.com>
2013-11-07 12:42:08 -0800

    commit new connection class
    MySqlSchema

---
+++ src/com/utube/dao/MySqlSchema.java
@@ -0,0 +1,127 @@
+package com.utube.dao;
+
+import java.sql.*;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.utube.util.ToJSON;
+
+/**
+ * This java class will hold all the sql queries from episode 5 and onward.
+ * V1_inventory.java and V1_status.java will not use this class for its sql code
+ * since they were created before episode 5.
+ * 
+ * Having all sql/database code in one package makes it easier to maintain and
+ * audit but increase complexity.
+ * 
+ * Note: we also used the extends Oracle308tube on this java class to inherit
+ * all the methods in Oracle308tube.java
+ * 
+ * @author 308tube
+ */
+
+public class MySqlSchema extends MySqlDataSource {
+
+	/**
+	 * Method searches for a specific brand from the PC_PARTS table. By using
+	 * prepareStatement and the ?, we protect against sql injection
+	 * 
+	 * Never add parameter straight into the prepareStatement
+	 * 
+	 * @param brand
+	 *            - product brand
+	 * @return - json array of the results from the database
+	 * @throws Exception
+	 */
+	public JSONArray queryReturnBrandParts(String brand) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+
+		try {
+			conn =  mysqlPcPartsConnection();
+			query = conn
+					.prepareStatement("select PC_PARTS_PK, PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC "
+							+ "from PC_PARTS "
+							+ "where UPPER(PC_PARTS_MAKER) = ? ");
+
+			query.setString(1, brand.toUpperCase()); // protect against sql
+														// injection
+			ResultSet rs = query.executeQuery();
+
+			json = converter.toJSONArray(rs);
+			query.close(); // close connection
+		} catch (SQLException sqlError) {
+			sqlError.printStackTrace();
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return json;
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+		return json;
+	}
+
+	/**
+	 * This method will search for the specific brand and the brands item number
+	 * in the PC_PARTS table.
+	 * 
+	 * By using prepareStatement and the ?, we are protecting against sql
+	 * injection
+	 * 
+	 * Never add parameter straight into the prepareStatement
+	 * 
+	 * @param brand
+	 *            - product brand
+	 * @param item_number
+	 *            - product item number
+	 * @return - json array of the results from the database
+	 * @throws Exception
+	 */
+	public JSONArray queryReturnBrandItemNumber(String brand, int item_number)
+			throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		ToJSON converter = new ToJSON();
+		JSONArray json = new JSONArray();
+
+		try {
+			conn =  mysqlPcPartsConnection();
+			query = conn
+					.prepareStatement("select PC_PARTS_PK, PC_PARTS_TITLE, PC_PARTS_CODE, PC_PARTS_MAKER, PC_PARTS_AVAIL, PC_PARTS_DESC "
+							+ "from PC_PARTS "
+							+ "where UPPER(PC_PARTS_MAKER) = ? "
+							+ "and PC_PARTS_CODE = ?");
+
+			/*
+			 * protect against sql injection when you have more than one ?, it
+			 * will go in chronological order.
+			 */
+			query.setString(1, brand.toUpperCase()); // first ?
+			query.setInt(2, item_number); // second ?
+			ResultSet rs = query.executeQuery();
+
+			json = converter.toJSONArray(rs);
+			query.close(); // close connection
+		} catch (SQLException sqlError) {
+			sqlError.printStackTrace();
+			return json;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return json;
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+		return json;
+	}
+}
--- src/com/utube/rest/inventory/V2_inventory.java
+++ src/com/utube/rest/inventory/V2_inventory.java
@@ -1,7 +1,152 @@
 package com.utube.rest.inventory;
 
-public class V2_inventory {
-	
-	
 
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.utube.dao.MySqlSchema;
+
+/**
+ * This class is used to manage computer parts inventory. It improves
+ * V1_inventory.java
+ * 
+ * @author 308tube
+ */
+@Path("/v2/inventory")
+public class V2_inventory {
+
+	/**
+	 * This method returns the specific brand of PC parts the user is looking for.  
+	 * It uses a QueryParam bring in the data to the method.
+	 * 
+	 * Example would be:
+	 * http://localhost:7001/com.youtube.rest/api/v2/inventory?brand=ASUS
+	 * 
+	 * @param brand - product brand name
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrandParts(
+				@QueryParam("brand") String brand)
+				throws Exception {
+		
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			//return a error is brand is missing from the url string
+			if(brand == null) {
+				return Response.status(400).entity("Error: please specify brand for this search").build();
+			}
+			
+			MySqlSchema dao = new MySqlSchema();
+			
+			json = dao.queryReturnBrandParts(brand);
+			returnString = json.toString();
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+	/*
+	 * This method can be used if the method returnBrandParts is not used.
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception {
+		
+		return Response.status(400).entity("Error: please specify brand for this search").build();
+	}
+	*/
+	
+	/**
+	 * This method will return the specific brand of PC parts the user is looking for.
+	 * It is very similar to the method returnBrandParts except this method uses the 
+	 * PathParam to bring in the data.
+	 * 
+	 * Example would be:
+	 * http://localhost:7001/com.youtube.rest/api/v2/inventory/ASUS
+	 * 
+	 * @param brand - product brand name
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+	@Path("/{brand}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrand(
+				@PathParam("brand") String brand) 
+				throws Exception {
+		
+		String returnString = null;
+		
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			MySqlSchema dao = new MySqlSchema();
+			
+			json = dao.queryReturnBrandParts(brand);
+			returnString = json.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+	/**
+	 * This method does a search on both product and the product item number.
+	 * It uses PathParam to bring in both parameters.
+	 * 
+	 * Example:
+	 * http://localhost:7001/com.youtube.rest/api/v2/inventory/ASUS/168131318
+	 * 
+	 * @param brand - product brand name
+	 * @param item_number - product item number
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+	@Path("/{brand}/{item_number}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnSpecificBrandItem(
+				@PathParam("brand") String brand,
+				@PathParam("item_number") int item_number) 
+				throws Exception {
+		
+		String returnString = null;
+		
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			MySqlSchema dao = new MySqlSchema();
+			
+			json = dao.queryReturnBrandItemNumber(brand, item_number);
+			returnString = json.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
 }
